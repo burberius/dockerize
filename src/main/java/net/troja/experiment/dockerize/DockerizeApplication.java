78831318dbabb82906569af051ef844093e2ba69
Jens Oberender <burberius@users.noreply.github.com>
2019-08-23 22:47:59 +0200

    Added source for service.

---
+++ src/main/java/net/troja/experiment/dockerize/DockerizeApplication.java
@@ -0,0 +1,13 @@
+package net.troja.experiment.dockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/troja/experiment/dockerize/HelloWorldController.java
@@ -0,0 +1,18 @@
+package net.troja.experiment.dockerize;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloWorldController {
+    private AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                "Hello " + name);
+    }
+}
